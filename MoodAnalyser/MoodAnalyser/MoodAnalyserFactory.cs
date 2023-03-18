6c05425a046596222c4605d25e6143ab2ef042bf
Princekotadiya007 <princuuu77@gmail.com>
2023-03-18 17:45:33 +0530

    [prince] Reflection create obj

---
+++ MoodAnalyser/MoodAnalyser/MoodAnalyserFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyserFactory
+    {
+        //Method to create Mood Analyser Object Using Reflection
+        public static object CreateMoodAnalyser(string className, string constructorName)
+        {
+            //Matching the pattern for extension of namespace
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnlyseException(MoodAnlyseException.MoodAnlyseExceptionType.NO_SUCH_CLASS, "Class not found");
+
+                }
+            }
+            else
+            {
+                throw new MoodAnlyseException(MoodAnlyseException.MoodAnlyseExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+            }
+        }
+    }
+}
