03ca4f2dc1a0b0c3377fe8b71e6108e07103a383
Princekotadiya007 <princuuu77@gmail.com>
2023-03-18 18:12:13 +0530

    [prince] class name return object

---
+++ MoodAnalyzerTest/ReflectionTestCase.cs
@@ -0,0 +1,28 @@
+﻿using MoodAnalyser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerTest
+{
+    public class ReflectionTestCase
+    {
+        [Test]
+        public void Return_Obj()
+        {
+            try
+            {
+                string message = null;
+                object expected = new MoodAnalyzerProblem(message);
+                object obj = MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyzerProblem.MoodAnlyseException", "MoodAnalyzerProblem");
+                expected.Equals(obj);
+            }  
+            catch (MoodAnlyseException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
